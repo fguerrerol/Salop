0f950e4be92382f4d958d9cdc8e151e69342fdba
Francisco Guerrero <fguerrerolopez@udesa.edu.ar>
2022-02-14 05:34:02 -0300

    Cambio de carpetas
    Cambio de carpetas , creación de carpeta de códigos para tener todo más organizado

---
+++ Códigos/Tasas_optimas.jl
@@ -0,0 +1,106 @@
+using PlotlyJS
+
+
+tau_p = collect(1:0.01:2)
+tau_c = collect(1:0.01:2)
+
+n = 10
+A = 1.5
+delta = 0.05
+rho = 0.1
+
+r_p(tau_p,tau_c,delta) = ((A^2)/(2*(tau_c + delta)*sqrt(tau_p - (1/(2*n)))))*(
+    ( sqrt(tau_c + delta)-sqrt(tau_p - (1/(2*n))))/(3) + tau_p/(2*sqrt(tau_p - (1/(2*n))))
+) + rho
+
+
+
+
+
+
+
+layout_per = Layout(title="Tasa de la firma del perimetro",
+scene=attr(
+    yaxis_title ="Cost-Per",
+    xaxis_title ="Cost-Centro ",
+    zaxis_title="Tasa de el banco",
+    camera_eye=attr(x=2, y=2, z=0.5)))
+
+tasa_per_surface = plot(surface(z= r_p.(tau_p',tau_c), x= tau_c, y = tau_p),layout_per)
+savefig(tasa_per_surface,"Tasa_perimetro.png")
+
+layout = Layout(title="Tasa de la firma del centro",
+scene=attr(
+    yaxis_title ="Cost-Per",
+    xaxis_title ="Cost-Centro ",
+    zaxis_title="Tasa de el banco",
+    camera_eye=attr(x=2, y=2, z=0.5)))
+
+r_c(tau_p,tau_c) = ((A^2)/(2*(tau_c + delta)*sqrt(tau_p - (1/(2*n)))))*(
+    (1/(4*n*sqrt(tau_p - (1/(2*n)))))
+    -( sqrt(tau_c + delta)-sqrt(tau_p - (1/(2*n))))/(3) + (tau_p*sqrt(tau_p - (1/(2*n))))/(2*(tau_c+delta))
+) + rho
+
+
+tasa_ctr_surface = plot(surface(z= r_c.(tau_p',tau_c), x= tau_c, y = tau_p),layout)
+
+delta1 = 0.05
+r_p_1(tau_p,tau_c) = ((A^2)/(2*(tau_c + delta1)*sqrt(tau_p - (1/(2*n)))))*(
+    ( sqrt(tau_c + delta1)-sqrt(tau_p - (1/(2*n))))/(3) + tau_p/(2*sqrt(tau_p - (1/(2*n))))
+) + rho
+
+delta2=0.10
+r_p_2(tau_p,tau_c) = ((A^2)/(2*(tau_c + delta2)*sqrt(tau_p - (1/(2*n)))))*(
+    ( sqrt(tau_c + delta2)-sqrt(tau_p - (1/(2*n))))/(3) + tau_p/(2*sqrt(tau_p - (1/(2*n))))
+) + rho
+
+delta3= 0.15
+r_p_3(tau_p,tau_c) = ((A^2)/(2*(tau_c + delta3)*sqrt(tau_p - (1/(2*n)))))*(
+    ( sqrt(tau_c + delta3)-sqrt(tau_p - (1/(2*n))))/(3) + tau_p/(2*sqrt(tau_p - (1/(2*n))))
+) + rho
+
+
+fig = make_subplots(
+
+    rows=1, cols=3,
+
+    specs=fill(Spec(kind="scene"), 1, 3),
+    
+
+    subplot_titles = ["Delta = 0.05" "Delta = 0.1" "Delta = 0.15"]
+)
+add_trace!(
+
+    fig,
+
+    surface(z= r_p_1.(tau_p',tau_c), x= tau_c, y = tau_p,showscale=false,Layout=layout),
+
+    row=1, col=1)
+
+add_trace!(
+
+    fig,
+
+    surface(z= r_p_2.(tau_p',tau_c), x= tau_c, y = tau_p,showscale=false),
+
+    row=1, col=2)
+add_trace!(
+
+    fig,
+
+    surface(z= r_p_3.(tau_p',tau_c), x= tau_c, y = tau_p,showscale=false),
+
+    row=1, col=3)
+relayout!(
+
+    fig,
+
+    title_text="3D subplots with different colorscales",
+
+    height=400,
+
+    width=800
+
+)
+
+fig
